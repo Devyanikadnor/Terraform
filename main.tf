b9e1c3a27474592ff5529e4e2bae139ed74d129c
Devyanikadnor <146559702+Devyanikadnor@users.noreply.github.com>
2026-01-29 05:24:51 +0000

    add

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
-resource "aws_s3_bucket" "my_bucket" {
+/*resource "aws_s3_bucket" "my_bucket" {
   bucket = "devyani-terraform-demo-bucket-12345"  # MUST be globally unique
 
   tags = {
     Name        = "Terraform-S3"
     Environment = "Dev"
+  }
+}*/
+
+resource "aws_instance" "vm" {
+  ami           = "ami-0ef44b9f9f20f3e57"
+  instance_type = "t3.micro"
+  key_name      = "vpc_key"
+  tags = {
+    Name = "terraform"
   }
 }
